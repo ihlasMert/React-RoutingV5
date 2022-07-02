c3ccc4bcf16b807b0fd7e65411276da65c880041
İHLASMERT <ihlas1mert@gmail.com>
2022-07-02 10:42:03 +0300

    added redirect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route,Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import MainHeader from "./components/MainHeader";
@@ -10,15 +10,19 @@ function App() {
       <MainHeader />
       <main>
         <Switch>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products" exact>
-          <Products />
-        </Route>
-        <Route path="/product-detail/:productId">
-          <ProductDetail />
-        </Route>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
+
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/product-detail/:productId">
+            <ProductDetail />
+          </Route>
         </Switch>
       </main>
     </div>
