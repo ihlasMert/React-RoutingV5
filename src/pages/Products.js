a55f0a3c9df8d78467b945c962dc7a6579580d9a
İHLASMERT <ihlas1mert@gmail.com>
2022-07-02 10:31:05 +0300

    added switch

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
-  return <h1>The Products Page</h1>;
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <li>
+          <Link to="/products">A book</Link>
+        </li>
+        <li>
+          <Link>A carpet</Link>
+        </li>
+        <li>
+          <Link>An Online Course</Link>
+        </li>
+      </ul>
+    </section>
+  );
 };
 export default Products;
-
